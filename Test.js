ee0276388ae5751559161be532477e8fc428a987
Vikash Kumar <vikashraj041013@gmail.com>
2022-07-27 08:54:46 +0530

    v1

---
+++ Test.js
@@ -0,0 +1,4 @@
+const json = '{"result":true, "count":42}';
+const obj = JSON.parse(json);
+console.log(obj)
+console.log(JSON.stringify(obj))
---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+// const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+const rootDir = require("./util/path");
+const errorController = require('./controllers/error')
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// res.status(404).sendFile(path.join(rootDir,'views','page-not-found.html'))
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+// app.engine('hbs', hbs.engine({
+//   extname: 'hbs',
+//   defaultLayout: 'layouts',
+//   layoutsDir: __dirname + '/views/layouts/',
+//   partialsDir: __dirname + '/views/partials'
+// }))
+
+// app.set('view engine','pug');
+// app.set('views','views')
